ee2cc7654cb37d6bfdfe697b1d4297060ae2554e
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-10-02 19:14:47 -0300

    feat: created modal with static data

---
+++ script.js
@@ -0,0 +1,18 @@
+let cards = [];
+
+window.onload = function () {
+  cards = document.getElementsByClassName("movie");
+
+  let buttonCloseModal = document.getElementById("modal__close");
+  buttonCloseModal.addEventListener("click", toggleModal);
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", toggleModal);
+  }
+};
+
+function toggleModal() {
+  let modal = document.getElementById("demo-modal");
+  document.body.classList.toggle("body__modal-opened");
+  modal.classList.toggle("modal__target");
+}
