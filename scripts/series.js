91870b2b0b307076a1cb0965a5319437ee845b4e
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-10-12 14:44:26 -0300

    feat: add series list and page title

---
+++ scripts/series.js
@@ -0,0 +1,102 @@
+const series = [
+  {
+    backdrop_path: "/q3jHCb4dMfYF6ojikKuHd6LscxC.jpg",
+    id: 84958,
+    name: "Loki",
+    overview:
+      "Começando imediatamente depois que Loki rouba o Tesseract (de novo), ele se encontra diante da Autoridade de Variação Temporal, uma organização burocrática que existe fora do tempo e espaço. Forçado a responder por seus crimes contra a linha do tempo, ele recebe uma escolha: Ser deletado da realidade ou ajudar a capturar uma ameaça ainda maior.",
+    poster_path: "/ukR0MkCQE6IylzBPd61txJi1L3E.jpg",
+    genre_ids: [18, 10765],
+    first_air_date: "2021-06-09",
+  },
+  {
+    backdrop_path: "/4DxHcIQfw3ungfQVqupJ8K62M3d.jpg",
+    id: 72710,
+    name: "O Continental: Do Mundo de John Wick",
+    overview:
+      "O jovem Winston Scott é arrastado para o cenário infernal da Nova York dos anos 70 para enfrentar um passado que ele pensava ter deixado para trás. Winston traça um caminho mortal através do misterioso submundo do hotel em uma tentativa angustiante de tomá-lo e finalmente assumir seu trono.",
+    poster_path: "/uRcrRc74ztb1TV5uojRtXqvNQ5y.jpg",
+    genre_ids: [80, 10759],
+    first_air_date: "2023-09-22",
+  },
+  {
+    backdrop_path: "/mvoW41kdSxiobGZ9ONL1Tqrpt3h.jpg",
+    id: 114461,
+    name: "Ahsoka",
+    overview:
+      "Situada após a queda do Império, a série segue a ex-cavaleira Jedi Ahsoka Tano enquanto ela investiga uma ameaça emergente a uma galáxia vulnerável.",
+    poster_path: "/laCJxobHoPVaLQTKxc14Y2zV64J.jpg",
+    genre_ids: [10765, 10759],
+    first_air_date: "2023-08-22",
+  },
+  {
+    backdrop_path: "/aY7zv2pfk9H0QxaaL3PBjvalbKQ.jpg",
+    id: 96677,
+    name: "Lupin",
+    overview:
+      'Na adolescência, a vida de Assane Diop dá uma guinada radical quando seu pai morre após ser acusado de um crime que não cometeu. Vinte e cinco anos depois, Assane se inspira em "Arsène Lupin, o Ladrão de Casaca" para vingá-lo.',
+    poster_path: "/yUSpKDhibrxnY3EXAcNkaPrUOI0.jpg",
+    genre_ids: [80, 18, 9648],
+    first_air_date: "2021-01-08",
+  },
+  {
+    backdrop_path: "/aCROH2L1yTeRe3im0XOYiUbI2YJ.jpg",
+    id: 205715,
+    name: "Gen V",
+    overview:
+      "Alunos talentosos põem os limites morais à prova, competindo pelo melhor lugar no ranking da universidade e uma chance de se juntar aos Sete, a equipe de elite da Vought. Quando os segredos obscuros da universidade vêm à tona, eles devem decidir que tipo de heróis querem ser.",
+    poster_path: "/wGOV8WrYzHKYt9lMrSGzxVAlfqL.jpg",
+    genre_ids: [10765, 10759, 18],
+    first_air_date: "2023-09-28",
+  },
+  {
+    backdrop_path: "/4Mt7WHox67uJ1yErwTBFcV8KWgG.jpg",
+    id: 37854,
+    name: "One Piece",
+    overview:
+      'Houve um homem que conquistou tudo aquilo que o mundo tinha a oferecer, o lendário Rei dos Piratas, Gold Roger. Capturado e condenado à execução pelo Governo Mundial, suas últimas palavras lançaram legiões aos mares. "Meu tesouro? Se quiserem, podem pegá-lo. Procurem-no! Ele contém tudo que este mundo pode oferecer!". Foi a revelação do maior tesouro, o One Piece, cobiçado por homens de todo o mundo, sonhando com fama e riqueza imensuráveis... Assim começou a Grande Era dos Piratas!',
+    poster_path: "/yBYDzzmNoT5uXkXo3NGatLIANYK.jpg",
+    genre_ids: [10759, 35, 16],
+    first_air_date: "1999-10-20",
+  },
+  {
+    backdrop_path: "/oahCTL082gwu6fYrxEOBe546uvB.jpg",
+    id: 71914,
+    name: "A Roda do Tempo",
+    overview:
+      "As vidas de cinco jovens camponeses mudam para sempre quando uma mulher estranha e poderosa chega dizendo que um deles é a criança de uma antiga profecia, com o poder de alterar o equilíbrio entre Luz e Trevas para sempre. Eles precisam decidir se confiam na desconhecida – e uns nos outros – para mudar o destino do mundo antes que o Tenebroso consiga se libertar de sua prisão.",
+    poster_path: "/lH1ZRIUQEXvWhY19EjOAGtr9aRc.jpg",
+    genre_ids: [10765, 18],
+    first_air_date: "2021-11-18",
+  },
+  {
+    backdrop_path: "/5SEEBS5qXgL5rgivTiAROy1Qt2q.jpg",
+    id: 81356,
+    name: "Sex Education",
+    overview:
+      "O inseguro Otis manja tudo quando o negócio é aconselhamento sexual, graças à sua mãe sexóloga. E aí a rebelde Maeve propõe criar uma clínica de terapia sexual na escola.",
+    poster_path: "/cK6JhcZFpwVW9tldz3llodY2TeY.jpg",
+    genre_ids: [35, 18],
+    first_air_date: "2019-01-11",
+  },
+  {
+    backdrop_path: "/reZvkJOe9HCyjSxMbmYwadg7S2K.jpg",
+    id: 120089,
+    name: "SPY x FAMILY",
+    overview:
+      'Há décadas, as nações de Ostania e Westalis promovem uma guerra fria sem fim. Para investigar os movimentos do presidente de um importante partido político, Westalis mobiliza Twilight, seu melhor agente, a montar uma família falsa e se infiltrar nos eventos sociais promovidos pela escola do filho do político. Mas por um acaso do destino, Twilight acaba adotando Anya, uma jovem com poderes telepáticos, e se "casando" com Yor, uma assassina profissional! Sem saberem das identidades uns dos outros, este trio incomum vai embarcar em aventuras cheias de surpresas para garantir a paz mundial.',
+    poster_path: "/7NAvPYPAu7MeHwP8E9sn81PqsRh.jpg",
+    genre_ids: [16, 35, 10759],
+    first_air_date: "2022-04-09",
+  },
+  {
+    backdrop_path: "/aRKQdF6AGbhnF9IAyJbte5epH5R.jpg",
+    id: 111110,
+    name: "ONE PIECE: A Série",
+    overview:
+      "O jovem pirata Monkey D. Luffy veste o chapéu de palha e, com sua tripulação atrapalhada, zarpa em uma jornada épica nesta adaptação em live-action do popular mangá.",
+    poster_path: "/y0VSmak9mSobl9QTKCvR7w4w6Ui.jpg",
+    genre_ids: [10759, 10765],
+    first_air_date: "2023-08-31",
+  },
+];
