bf9f02ece92e508a587e842b94afbf4dfa058b04
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-10-12 14:45:21 -0300

    feat: add series genres to list

--- scripts/genres.js
+++ scripts/genres.js
@@ -1,19 +1,43 @@
 const genres = [
   {
-    id: 28,
-    name: "Ação",
-  },
-  {
     id: 12,
     name: "Aventura",
+  },
+  {
+    id: 14,
+    name: "Fantasia",
   },
   {
     id: 16,
     name: "Animação",
   },
   {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 27,
+    name: "Terror",
+  },
+  {
+    id: 28,
+    name: "Ação",
+  },
+  {
     id: 35,
     name: "Comédia",
+  },
+  {
+    id: 36,
+    name: "História",
+  },
+  {
+    id: 37,
+    name: "Faroeste",
+  },
+  {
+    id: 53,
+    name: "Thriller",
   },
   {
     id: 80,
@@ -24,55 +48,63 @@ const genres = [
     name: "Documentário",
   },
   {
-    id: 18,
-    name: "Drama",
-  },
-  {
-    id: 10751,
-    name: "Família",
-  },
-  {
-    id: 14,
-    name: "Fantasia",
-  },
-  {
-    id: 36,
-    name: "História",
-  },
-  {
-    id: 27,
-    name: "Terror",
-  },
-  {
-    id: 10402,
-    name: "Música",
+    id: 878,
+    name: "Ficção científica",
   },
   {
     id: 9648,
     name: "Mistério",
   },
   {
+    id: 10402,
+    name: "Música",
+  },
+  {
     id: 10749,
     name: "Romance",
   },
   {
-    id: 878,
-    name: "Ficção científica",
-  },
-  {
-    id: 10770,
-    name: "Cinema TV",
-  },
-  {
-    id: 53,
-    name: "Thriller",
+    id: 10751,
+    name: "Família",
   },
   {
     id: 10752,
     name: "Guerra",
   },
   {
-    id: 37,
-    name: "Faroeste",
+    id: 10759,
+    name: "Action & Adventure",
+  },
+  {
+    id: 10762,
+    name: "Kids",
+  },
+  {
+    id: 10763,
+    name: "News",
+  },
+  {
+    id: 10764,
+    name: "Reality",
+  },
+  {
+    id: 10765,
+    name: "Sci-Fi & Fantasy",
+  },
+  {
+    id: 10766,
+    name: "Soap",
+  },
+  {
+    id: 10767,
+    name: "Talk",
+  },
+  {
+    id: 10768,
+    name: "War & Politics",
+  },
+  {
+    id: 10770,
+    name: "Cinema TV",
   },
 ];
