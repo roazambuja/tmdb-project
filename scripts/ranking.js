ddd32ff20aedc5e05f2484cbfe9624e4b183a253
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-10-29 22:57:03 -0300

    feat: created ranking constructor

---
+++ scripts/ranking.js
@@ -0,0 +1,42 @@
+function Ranking() {
+  this.selected = "movies";
+
+  let movieButton = document.getElementById("movies");
+  movieButton.addEventListener("click", () => this.changeType("movies"));
+
+  let seriesButton = document.getElementById("series");
+  seriesButton.addEventListener("click", () => this.changeType("series"));
+
+  let pageTitle = document.getElementById("title");
+
+  this.changeType = function (type) {
+    if (this.selected != type) {
+      this.selected = type;
+
+      const element = document.getElementById("cards");
+      while (element.firstChild) {
+        element.removeChild(element.firstChild);
+      }
+
+      this.renderItens();
+    }
+  };
+
+  this.renderItens = function () {
+    let list = this.selected == "movies" ? movies : series;
+    pageTitle.innerText = "Top 10 - " + (this.selected == "movies" ? "Filmes" : "Séries");
+
+    list.forEach((item) => {
+      let i = new Movie(
+        item.backdrop_path,
+        item.genre_ids,
+        item.id,
+        item.overview,
+        item.poster_path,
+        this.selected == "movies" ? item.release_date : item.first_air_date,
+        this.selected == "movies" ? item.title : item.name
+      );
+      i.render();
+    });
+  };
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,4 @@
 window.onload = function () {
-  movies.forEach((movie) => {
-    let m = new Movie(
-      movie.backdrop_path,
-      movie.genre_ids,
-      movie.id,
-      movie.overview,
-      movie.poster_path,
-      movie.release_date,
-      movie.title
-    );
-    m.render();
-  });
+  let ranking = new Ranking();
+  ranking.renderItens();
 };
