a8322334e55b6c624d4dcf7eb9a6461f37ddfe02
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-11-01 22:32:10 -0300

    feat: add api url to a const

--- scripts/script.js
+++ scripts/script.js
@@ -8,22 +8,24 @@ const options = {
   },
 };
 
+const api = "https://api.themoviedb.org/3";
+
 function getMovies() {
-  return fetch("https://api.themoviedb.org/3/trending/movie/week?language=pt-BR", options)
+  return fetch(`${api}/trending/movie/week?language=pt-BR`, options)
     .then((response) => response.json())
     .then((response) => response.results.slice(0, 10))
     .catch((err) => console.error(err));
 }
 
 function getMovieCrew(id) {
-  return fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=pt-BR`, options)
+  return fetch(`${api}/movie/${id}/credits?language=pt-BR`, options)
     .then((response) => response.json())
     .then((response) => response.crew)
     .catch((err) => console.error(err));
 }
 
 function getMovieDetails(id) {
-  return fetch(`https://api.themoviedb.org/3/movie/${id}?language=pt-BR`, options)
+  return fetch(`${api}/movie/${id}?language=pt-BR`, options)
     .then((response) => response.json())
     .catch((err) => console.error(err));
 }
