8f430b82bb4a24f51e66989ea9bd270f3d65dce7
Roberta Azambuja <robertaazambuja01@gmail.com>
2023-11-18 12:38:00 -0300

    feat: added loading and error message

--- scripts/ranking.js
+++ scripts/ranking.js
@@ -1,15 +1,17 @@
 function Ranking() {
   this.selected = "movies";
+  this.list = [];
+  this.cardList = [];
 
   let movieButton = document.getElementById("movies");
-  movieButton.addEventListener("click", () => this.changeType("movies"));
+  movieButton.addEventListener("click", async () => await this.changeType("movies"));
 
   let seriesButton = document.getElementById("series");
-  seriesButton.addEventListener("click", () => this.changeType("series"));
+  seriesButton.addEventListener("click", async () => await this.changeType("series"));
 
   let pageTitle = document.getElementById("title");
 
-  this.changeType = function (type) {
+  this.changeType = async function (type) {
     if (this.selected != type) {
       this.selected = type;
 
@@ -18,25 +20,36 @@ function Ranking() {
         element.removeChild(element.firstChild);
       }
 
-      this.renderItens();
+      await this.renderItens();
     }
   };
 
   this.renderItens = async function () {
-    let list = this.selected == "movies" ? await getMovies() : await getSeries();
-    pageTitle.innerText = "Top 10 - " + (this.selected == "movies" ? "Filmes" : "Séries");
+    let message = document.createElement("p");
+    let cards = document.getElementById("cards");
+    message.classList.add("message");
+    cards.appendChild(message);
 
-    list.forEach((item) => {
-      let i = new Movie(
-        item.backdrop_path,
-        item.id,
-        item.overview,
-        item.poster_path,
-        this.selected == "movies" ? item.release_date : item.first_air_date,
-        this.selected == "movies" ? item.title : item.name,
-        this.selected == "movies" ? "movies" : "series"
-      );
-      i.render();
-    });
+    try {
+      message.innerText = "Carregando informações...";
+      this.list = this.selected == "movies" ? await getMovies() : await getSeries();
+      pageTitle.innerText = "Top 10 - " + (this.selected == "movies" ? "Filmes" : "Séries");
+
+      this.list.forEach(async (item) => {
+        let m = new Movie(
+          item.backdrop_path,
+          item.id,
+          item.overview,
+          item.poster_path,
+          this.selected == "movies" ? item.release_date : item.first_air_date,
+          this.selected == "movies" ? item.title : item.name,
+          this.selected == "movies" ? "movies" : "series"
+        );
+        await m.render();
+      });
+      cards.removeChild(message);
+    } catch (e) {
+      message.innerText = "Ocorreu um erro ao carregar os dados.";
+    }
   };
 }
